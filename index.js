dfbf713eb1574effd6135de00002ce89a494bbff
Maxis Kao <maxiskao@yahoo-inc.com>
2015-12-14 18:09:41 +0800

    add API interface

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var app = express();
 
 // view engine setup
 app.engine('.tpl', exphbs({
-	defaultLayout: 'single',
-	extname: '.tpl'
+    defaultLayout: 'single',
+    extname: '.tpl'
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,13 +18,39 @@ app.set('view engine', '.tpl');
 
 // server route
 app.get('/', function(req, res, next) {
-	res.render('index', {
-		title: 'Timelapse News'
-	});
+    res.render('index', {
+        title: 'Timelapse News'
+    });
+});
+
+app.get('/api/search/topic/:topic', function(req, res, next) {
+    // mock data for backend
+    // ...
+
+    var response = {
+        topic: req.params.topic,
+        events: [
+            {
+                'title': 'kerker',
+                'abstract': 'ker ker ker'
+            }
+        ]
+    };
+    return res.send(JSON.stringify(response));
+});
+
+app.get('/api/search/query/:query', function(req, res, next) {
+    // mock data for backend
+    // ...
+    var response = {
+        input: req.params.query,
+        suggestion: req.params.query + 'kerker'
+    };
+    return res.send(JSON.stringify(response));
 });
 
 app.get('/test', function(req, res) {
-	res.render('test');
+    res.render('test');
 });
 
 app.set('port', (process.env.PORT || 3000));
